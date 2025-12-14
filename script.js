ea6bf3588d14c8a476483117de1b243b7f41a053
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-12-14 23:12:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ const fileNames = [
 
 // do not change the code above
 // add your code here
+files.forEach(file => {
+    fetch(file)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text(); // Read the response as text
+        })
+        .then(data => {
+            console.log(data); // Log the content of the file
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+});
