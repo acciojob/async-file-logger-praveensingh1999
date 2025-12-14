9f33b7795b686fe7c37c8fdda3a8314ca60d434e
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-12-14 23:14:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-//your JS code here. If required.
-const fileNames = [
+// Given array (already provided in your file)
+const files = [
   "file1.txt",
   "file2.txt",
   "file3.txt",
   "file4.txt",
-  "file5.txt",
+  "file5.txt"
 ];
 
-// do not change the code above
-// add your code here
-files.forEach(file => {
-    fetch(file)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text(); // Read the response as text
-        })
-        .then(data => {
-            console.log(data); // Log the content of the file
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
+// Read all files asynchronously
+Promise.all(
+  files.map(file =>
+    fetch(file).then(response => response.text())
+  )
+).then(contents => {
+  contents.forEach(content => {
+    console.log(content);
+  });
+}).catch(error => {
+  console.error("Error reading files:", error);
 });
